575bead15738162e012708e7aa89748238921e4e
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2026-01-02 23:08:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-// complete this js code
 function Person(name, age) {
-	constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+  this.name = name;
+  this.age = age;
 }
+
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
 function Employee(name, age, jobTitle) {
-	 constructor(name, age, jobTitle) {
-    super(name, age);
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(
-      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-    );
-  }
+  Person.call(this, name, age); // inherit properties
+  this.jobTitle = jobTitle;
 }
 
-// Do not change code below this line
+// Inherit methods from Person
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
 window.Person = Person;
 window.Employee = Employee;
